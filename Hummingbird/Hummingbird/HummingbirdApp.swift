bd6d9668cb67626000bacaed9d026b48b839c07e
debbieyuen <deborahyuen2016@berkeley.edu>
2023-02-17 21:36:49 -0800

    New App

---
+++ Hummingbird/Hummingbird/HummingbirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  HummingbirdApp.swift
+//  Hummingbird
+//
+//  Created by Debbie Yuen on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct HummingbirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
